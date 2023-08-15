473d90da7ad196d6592831f31fb65c149ea65af8
Harsh7113 <114785191+Harsh7113@users.noreply.github.com>
2023-08-15 13:02:56 +0530

    Update Stock Price Prediction.py

--- Stock Price Prediction.py
+++ Stock Price Prediction.py
@@ -62,7 +62,7 @@ model.add(Dense(25))
 model.add(Dense(1))
 
 model.compile(optimizer="adam",loss="mean_squared_error")
-model.fit(x_train,y_train,batch_size=1,epochs=5)
+model.fit(x_train,y_train,batch_size=1,epochs=1)
 
 #create the testing dataset
 test_data=scaled_data[training_data_len-60:,:]
